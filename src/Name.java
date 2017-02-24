1c9268a7d4c90815003ebf1739d383361f572c58
zzzlarry <105021018@live.asia.edu.tw>
2017-02-24 14:58:07 +0800

    hw submit

---
+++ src/Name.java
@@ -0,0 +1,15 @@
+/*
+* Topic:第一次作業自我介紹
+* Date: 2017/02/24
+* Author: 105021018 藍子朋
+*/
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("藍子朋");
+		System.out.println("105021018");
+		System.out.println("亞洲大學資工系資電組");
+	}
+
+}
